1f4b383bf2b81c508e08c7c1494569ad575861d5
Daniel Parra Riera <danielparrariera7@gmail.com>
2026-02-16 19:18:56 +0100

    Primera subida

---
+++ tarea_1.js
@@ -0,0 +1,8 @@
+const inventario=[
+    {"nombre":"Producto1", "precio": 120, "stock":2},
+    {"nombre":"Producto2", "precio": 140, "stock":3},
+    {"nombre":"Producto3", "precio": 80, "stock":1}
+]
+function agregarCarrito (nombreProducto, cantidadDeseada){
+
+}
